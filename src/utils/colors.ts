52029aef9074d69ac1778f318140f4ec762ea446
yukiao <lembang19sony@gmail.com>
2023-11-16 22:34:30 +0800

    feat: add styling package

---
+++ src/utils/colors.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  BLUE: "#02295a",
+  PURPLISH_BLUE: "#473dff",
+  PASTEL_BLUE: "#adbeff",
+  LIGHT_BLUE: "#bfe2fd",
+  STRAWBERRY_RED: "#ed3548",
+  COOL_GRAY: "#9699ab",
+  LIGHT_GRAY: "#d6d9e6",
+  MAGNOLIA: "#f0f6ff",
+  ALABASTER: "#fafbff",
+  WHITE: "#ffffff",
+};
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      BLUE: "#02295a",
+      PURPLISH_BLUE: "#473dff",
+      PASTEL_BLUE: "#adbeff",
+      LIGHT_BLUE: "#bfe2fd",
+      STRAWBERRY_RED: "#ed3548",
+      COOL_GRAY: "#9699ab",
+      LIGHT_GRAY: "#d6d9e6",
+      MAGNOLIA: "#f0f6ff",
+      ALABASTER: "#fafbff",
+      WHITE: "#ffffff",
+    },
+  },
+  plugins: [],
+};
